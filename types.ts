e0bdd429521994efa122b20306d70c02c8af5d9a
unknown <txr456@gmail.com>
2025-11-24 20:49:46 +0200

    [dyad] wrote 2 file(s)

--- types.ts
+++ types.ts
@@ -211,10 +211,3 @@ declare global {
     }
   }
 }
-
-// Augment ImportMetaEnv for Vite environment variables
-interface ImportMetaEnv {
-  readonly VITE_SUPABASE_URL: string;
-  readonly VITE_SUPABASE_ANON_KEY: string;
-  readonly GEMINI_API_KEY: string;
-}
